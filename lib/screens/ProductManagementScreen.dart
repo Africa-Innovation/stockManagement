d06d27d1c2e4d6188b9ed5b40d062a3bb837bfe9
Africa-Innovation <africa.innov2023@gmail.com>
2025-02-25 08:19:22 +0000

    jai add barre de recherche dans vente et produit

--- lib/screens/ProductManagementScreen.dart
+++ lib/screens/ProductManagementScreen.dart
@@ -1,9 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:stockmanagement/Model/product_model.dart';
 import '../database/database_helper.dart';
- 
+
 class ProductManagementScreen extends StatefulWidget {
   @override
   _ProductManagementScreenState createState() => _ProductManagementScreenState();
@@ -11,159 +9,186 @@ class ProductManagementScreen extends StatefulWidget {
 
 class _ProductManagementScreenState extends State<ProductManagementScreen> {
   late List<Product> products = [];
+  List<Product> _filteredProducts = [];
+  final TextEditingController _searchController = TextEditingController(); // Contrôleur de recherche
 
   @override
   void initState() {
     super.initState();
     _loadProducts();
+    _searchController.addListener(() {
+      _filterProducts(_searchController.text);
+    });
   }
 
   Future<void> _loadProducts() async {
-  final data = await DatabaseHelper.instance.getAllProducts();
-  print("Produits chargés : $data"); // Debug
-  setState(() {
-    products = data;
-  });
-}
+    final data = await DatabaseHelper.instance.getAllProducts();
+    setState(() {
+      products = data;
+      _filteredProducts = data;  // Initialiser la liste filtrée avec tous les produits
+    });
+  }
 
+  void _filterProducts(String query) {
+    final filteredList = products.where((product) {
+      return product.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    setState(() {
+      _filteredProducts = filteredList;
+    });
+  }
 
   void _showProductDialog({Product? product}) {
-  final nameController = TextEditingController(text: product?.name ?? '');
-  final quantityController = TextEditingController(text: product?.quantity.toString() ?? '');
-  final priceController = TextEditingController(text: product?.price.toString() ?? '');
-  final minStockController = TextEditingController(text: product?.minStock.toString() ?? '');
+    final nameController = TextEditingController(text: product?.name ?? '');
+    final quantityController = TextEditingController(text: product?.quantity.toString() ?? '');
+    final priceController = TextEditingController(text: product?.price.toString() ?? '');
+    final minStockController = TextEditingController(text: product?.minStock.toString() ?? '');
 
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text(product == null ? 'Ajouter un produit' : 'Modifier le produit'),
-      content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Nom')),
-            TextField(controller: quantityController, decoration: InputDecoration(labelText: 'Quantité'), keyboardType: TextInputType.number),
-            TextField(controller: priceController, decoration: InputDecoration(labelText: 'Prix'), keyboardType: TextInputType.number),
-            TextField(controller: minStockController, decoration: InputDecoration(labelText: 'Stock minimum'), keyboardType: TextInputType.number),
-          ],
-        ),
-      ),
-      actions: [
-        TextButton(onPressed: () => Navigator.pop(context), child: Text('Annuler')),
-        ElevatedButton(
-          onPressed: () async {
-            final name = nameController.text.trim();
-            final quantity = int.tryParse(quantityController.text);
-            final price = double.tryParse(priceController.text);
-            final minStock = int.tryParse(minStockController.text);
-
-            // Vérifier si tous les champs sont renseignés
-            if (name.isEmpty || quantity == null || price == null || minStock == null) {
-              _showErrorDialog('Tous les champs doivent être remplis correctement.');
-              return;
-            }
-
-            // Vérification si le stock minimum est supérieur à la quantité
-            if (minStock > quantity) {
-              _showErrorDialog('Le stock minimum ne peut pas être supérieur à la quantité.');
-              return;
-            }
-
-            final newProduct = Product(
-              id: product?.id,
-              name: name,
-              quantity: quantity,
-              price: price,
-              minStock: minStock,
-            );
-
-            if (product == null) {
-              await DatabaseHelper.instance.insertProduct(newProduct);
-            } else {
-              await DatabaseHelper.instance.updateProduct(newProduct);
-            }
-
-            _loadProducts();
-            Navigator.pop(context);
-          },
-          child: Text(product == null ? 'Ajouter' : 'Modifier'),
-        ),
-      ],
-    ),
-  );
-}
-
-void _showErrorDialog(String message) {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Erreur'),
-      content: Text(message),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: Text('OK'),
-        ),
-      ],
-    ),
-  );
-}
-
-
-  void _deleteProduct(int id) async {
-  // Afficher un dialogue de confirmation avant la suppression
-  bool? confirmDelete = await showDialog<bool>(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Confirmer la suppression'),
-        content: Text('Êtes-vous sûr de vouloir supprimer ce produit ?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false); // Annuler la suppression
-            },
-            child: Text('Annuler'),
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(product == null ? 'Ajouter un produit' : 'Modifier le produit'),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: nameController, decoration: InputDecoration(labelText: 'Nom')),
+              TextField(controller: quantityController, decoration: InputDecoration(labelText: 'Quantité'), keyboardType: TextInputType.number),
+              TextField(controller: priceController, decoration: InputDecoration(labelText: 'Prix'), keyboardType: TextInputType.number),
+              TextField(controller: minStockController, decoration: InputDecoration(labelText: 'Stock minimum'), keyboardType: TextInputType.number),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true); // Confirmer la suppression
+        ),
+        actions: [
+          TextButton(onPressed: () => Navigator.pop(context), child: Text('Annuler')),
+          ElevatedButton(
+            onPressed: () async {
+              final name = nameController.text.trim();
+              final quantity = int.tryParse(quantityController.text);
+              final price = double.tryParse(priceController.text);
+              final minStock = int.tryParse(minStockController.text);
+
+              if (name.isEmpty || quantity == null || price == null || minStock == null) {
+                _showErrorDialog('Tous les champs doivent être remplis correctement.');
+                return;
+              }
+
+              if (minStock > quantity) {
+                _showErrorDialog('Le stock minimum ne peut pas être supérieur à la quantité.');
+                return;
+              }
+
+              final newProduct = Product(
+                id: product?.id,
+                name: name,
+                quantity: quantity,
+                price: price,
+                minStock: minStock,
+              );
+
+              if (product == null) {
+                await DatabaseHelper.instance.insertProduct(newProduct);
+              } else {
+                await DatabaseHelper.instance.updateProduct(newProduct);
+              }
+
+              _loadProducts();
+              Navigator.pop(context);
             },
-            child: Text('Supprimer'),
+            child: Text(product == null ? 'Ajouter' : 'Modifier'),
           ),
         ],
-      );
-    },
-  );
-
-  // Si la suppression est confirmée, on supprime le produit
-  if (confirmDelete == true) {
-    await DatabaseHelper.instance.deleteProduct(id);
-    _loadProducts(); // Recharger la liste des produits
+      ),
+    );
   }
-}
 
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Erreur'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteProduct(int id) async {
+    bool? confirmDelete = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmer la suppression'),
+          content: Text('Êtes-vous sûr de vouloir supprimer ce produit ?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Supprimer'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmDelete == true) {
+      await DatabaseHelper.instance.deleteProduct(id);
+      _loadProducts();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Gestion des produits')),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return ListTile(
-            title: Text(product.name),
-            subtitle: Text('Stock: ${product.quantity} - Prix: ${product.price} \$'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(icon: Icon(Icons.edit), onPressed: () => _showProductDialog(product: product)),
-                IconButton(icon: Icon(Icons.delete), onPressed: () => _deleteProduct(product.id!)),
-              ],
+      appBar: AppBar(
+        title: Text('Gestion des produits'),
+      ),
+      body: Column(
+        children: [
+          // Champ de recherche
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: "Rechercher un produit",
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredProducts.length,
+              itemBuilder: (context, index) {
+                final product = _filteredProducts[index];
+                return ListTile(
+                  title: Text(product.name),
+                  subtitle: Text('Stock: ${product.quantity} - Prix: ${product.price} \$'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(icon: Icon(Icons.edit), onPressed: () => _showProductDialog(product: product)),
+                      IconButton(icon: Icon(Icons.delete), onPressed: () => _deleteProduct(product.id!)),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showProductDialog(),
--- lib/screens/SalesScreen.dart
+++ lib/screens/SalesScreen.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqlite_api.dart';
 import 'package:stockmanagement/Model/receipt_model.dart';
@@ -11,7 +8,6 @@ import 'package:stockmanagement/Model/product_model.dart';
 import 'package:stockmanagement/screens/ReceiptPreviewScreen.dart';
 import 'package:uuid/uuid.dart';
 
-
 class SaleScreen extends StatefulWidget {
   @override
   _SaleScreenState createState() => _SaleScreenState();
@@ -19,23 +15,37 @@ class SaleScreen extends StatefulWidget {
 
 class _SaleScreenState extends State<SaleScreen> {
   late List<Product> products = [];
+  List<Product> _filteredProducts = [];
   final Map<int, int> selectedProducts = {};  // key: productId, value: quantity
   double totalAmount = 0.0;
   final TextEditingController clientNameController = TextEditingController();  // Pour saisir le nom du client
-  //String generatedReceiptId = Uuid().v4();  // Remplacer la génération de l'ID par un UUID
-// Générer un identifiant unique pour le reçu
-final String receiptId = Uuid().v4(); 
+  final TextEditingController _searchController = TextEditingController();  // Contrôleur de recherche
+  final String receiptId = Uuid().v4(); 
 
   @override
   void initState() {
     super.initState();
     _loadProducts();
+    _searchController.addListener(() {
+      _filterProducts(_searchController.text);
+    });
   }
 
   Future<void> _loadProducts() async {
     final data = await DatabaseHelper.instance.getAllProducts();
     setState(() {
       products = data;
+      _filteredProducts = data;  // Initialiser la liste filtrée avec tous les produits
+    });
+  }
+
+  void _filterProducts(String query) {
+    final filteredList = products.where((product) {
+      return product.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    setState(() {
+      _filteredProducts = filteredList;
     });
   }
 
@@ -66,133 +76,130 @@ final String receiptId = Uuid().v4();
   }
 
   void _processSale() async {
-  String clientName = clientNameController.text.trim();
+    String clientName = clientNameController.text.trim();
 
-  if (clientName.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Veuillez entrer un nom de client')),
+    if (clientName.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Veuillez entrer un nom de client')),
+      );
+      return;
+    }
+
+    // Afficher la boîte de dialogue de confirmation
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmer la vente'),
+          content: Text('Êtes-vous sûr de vouloir valider cette vente pour le client "$clientName" ?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await _confirmSale(clientName);
+              },
+              child: Text('Confirmer'),
+            ),
+          ],
+        );
+      },
     );
-    return;
   }
 
-  // Afficher la boîte de dialogue de confirmation
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Confirmer la vente'),
-        content: Text('Êtes-vous sûr de vouloir valider cette vente pour le client "$clientName" ?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Annuler'),
-          ),
-          TextButton(
-            onPressed: () async {
-              Navigator.of(context).pop();
-              await _confirmSale(clientName);
-            },
-            child: Text('Confirmer'),
-          ),
-        ],
-      );
-    },
-  );
-}
+  Future<void> _confirmSale(String clientName) async {
+    final db = await DatabaseHelper.instance.database;
+    double totalAmount = 0.0;
 
-Future<void> _confirmSale(String clientName) async {
-  final db = await DatabaseHelper.instance.database;
-  double totalAmount = 0.0;
+    // Générer un ID unique pour le reçu (juste ici une fois)
+    final String receiptId = Uuid().v4();  // ID unique pour chaque vente
 
-  // Générer un ID unique pour le reçu (juste ici une fois)
-  final String receiptId = Uuid().v4();  // ID unique pour chaque vente
+    await db.transaction((txn) async {
+      for (var entry in selectedProducts.entries) {
+        final productId = entry.key;
+        final quantity = entry.value;
+        final product = products.firstWhere((p) => p.id == productId);
 
-  await db.transaction((txn) async {
-    for (var entry in selectedProducts.entries) {
-      final productId = entry.key;
-      final quantity = entry.value;
-      final product = products.firstWhere((p) => p.id == productId);
+        if (quantity > product.quantity) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Quantité supérieure au stock disponible pour ${product.name}')),
+          );
+          return;
+        }
 
-      if (quantity > product.quantity) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Quantité supérieure au stock disponible pour ${product.name}')),
+        product.quantity -= quantity;
+        await txn.update(
+          'products',
+          {'quantity': product.quantity},
+          where: 'id = ?',
+          whereArgs: [product.id],
         );
-        return;
       }
+    });
 
-      product.quantity -= quantity;
-      await txn.update(
-        'products',
-        {'quantity': product.quantity},
-        where: 'id = ?',
-        whereArgs: [product.id],
-      );
-    }
-  });
-
-  totalAmount = selectedProducts.entries.fold(0.0, (sum, entry) {
-    final product = products.firstWhere((p) => p.id == entry.key);
-    return sum + product.price * entry.value;
-  });
-
-  // Créer la liste des items du reçu
-  List<ReceiptItem> receiptItems = selectedProducts.entries.map((entry) {
-    final product = products.firstWhere((p) => p.id == entry.key);
-    return ReceiptItem(product: product, quantity: entry.value);
-  }).toList();
-
-  // Créer la vente avec l'ID du reçu généré
-  Sale sale = Sale(
-    id: DateTime.now().millisecondsSinceEpoch, 
-    clientName: clientName,
-    saleItems: selectedProducts.entries.map((entry) {
+    totalAmount = selectedProducts.entries.fold(0.0, (sum, entry) {
       final product = products.firstWhere((p) => p.id == entry.key);
-      return SaleItem(product: product, quantity: entry.value);
-    }).toList(),
-    totalPrice: totalAmount,
-    date: DateTime.now(),
-    receiptId: receiptId,  // Associer l'ID généré à la vente
-  );
+      return sum + product.price * entry.value;
+    });
 
-  // Ajouter la vente à la base de données
-  await DatabaseHelper.instance.insertSale(sale);
+    // Créer la liste des items du reçu
+    List<ReceiptItem> receiptItems = selectedProducts.entries.map((entry) {
+      final product = products.firstWhere((p) => p.id == entry.key);
+      return ReceiptItem(product: product, quantity: entry.value);
+    }).toList();
 
-  // Créer l'objet Receipt avec le même ID
-  final receipt = Receipt(
-    id: sale.id.toString(),
-    clientName: clientName,
-    items: receiptItems,
-    totalAmount: totalAmount,
-    date: DateTime.now().toString(),
-    saleId: sale.id!,
-    receiptId: receiptId,  // Utilisation de l'ID unique pour le reçu
-  );
+    // Créer la vente avec l'ID du reçu généré
+    Sale sale = Sale(
+      id: DateTime.now().millisecondsSinceEpoch, 
+      clientName: clientName,
+      saleItems: selectedProducts.entries.map((entry) {
+        final product = products.firstWhere((p) => p.id == entry.key);
+        return SaleItem(product: product, quantity: entry.value);
+      }).toList(),
+      totalPrice: totalAmount,
+      date: DateTime.now(),
+      receiptId: receiptId,  // Associer l'ID généré à la vente
+    );
 
-  // Naviguer vers la page de prévisualisation du reçu
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => ReceiptPreviewScreen(receipt: receipt),
-    ),
-  );
+    // Ajouter la vente à la base de données
+    await DatabaseHelper.instance.insertSale(sale);
 
-  // Vider la sélection après la vente
-  setState(() {
-    selectedProducts.clear();
-  });
+    // Créer l'objet Receipt avec le même ID
+    final receipt = Receipt(
+      id: sale.id.toString(),
+      clientName: clientName,
+      items: receiptItems,
+      totalAmount: totalAmount,
+      date: DateTime.now().toString(),
+      saleId: sale.id!,
+      receiptId: receiptId,  // Utilisation de l'ID unique pour le reçu
+    );
 
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text('Vente réussie !')),
-  );
+    // Naviguer vers la page de prévisualisation du reçu
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ReceiptPreviewScreen(receipt: receipt),
+      ),
+    );
 
-  _loadProducts();
-  clientNameController.clear();
-}
+    // Vider la sélection après la vente
+    setState(() {
+      selectedProducts.clear();
+    });
 
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Vente réussie !')),
+    );
 
-
+    _loadProducts();
+    clientNameController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -202,6 +209,17 @@ Future<void> _confirmSale(String clientName) async {
       ),
       body: Column(
         children: [
+          // Champ de recherche
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: "Rechercher un produit",
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
           // Champ pour saisir le nom du client
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -212,9 +230,9 @@ Future<void> _confirmSale(String clientName) async {
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: products.length,
+              itemCount: _filteredProducts.length,
               itemBuilder: (context, index) {
-                final product = products[index];
+                final product = _filteredProducts[index];
                 return ListTile(
                   title: Text(product.name),
                   subtitle: Text('Prix: ${product.price} \$'),
